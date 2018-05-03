05e5cc5fc4ed75951e71fe4b35a8ff7da10fc71b
liuzhen <https://git.duapp.com/appidfe0nslwn50>
2018-05-03 11:24:43 +0800

    lz-5-03

--- read_sinonwx/.svn/pristine/71/7149185f53c2ee18eb1b5b361299622e7639c7ee.svn-base
+++
@@ -1,9 +0,0 @@
-package com.sinontech.service;
-
-import java.util.List;
-
-import com.sinontech.modle.ArticleLabel;
-
-public interface ArticleLabelService {
-	public List<ArticleLabel> varListLabel(long articleId);
-}
--- read_sinonwx/src/com/sinontech/pub/sql/Actsql.java
+++
@@ -1,7 +0,0 @@
-package com.sinontech.pub.sql;
-
-
-public class Actsql {
-	public static String userlist ="select * from mclon_userinfo";
-	public static String userByname = "select * from _userinfo where name = ?";
-}
